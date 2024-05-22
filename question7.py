40fc144f6466995cc8d03afdc1a614a3b7767812
sruthysuji <153523151+sruthysuji@users.noreply.github.com>
2024-05-22 10:26:36 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -5,6 +5,18 @@ b=[4,5,6]
 c=[7,5,3,2,3]
 d=[2]
 e=[3]
+output = []
+output.append(a)
+output.append(b)
+nestedlist = []
+nestedlist.append(e)
+nestedlist2 = []
+nestedlist2.append(d)
+nestedlist2.append(nestedlist)
+final = c.copy()
+final.extend(nestedlist)
+output.append(final_list)
+print(output)
 
 # use these lists to create another list which will look like the list output in line 2
 
