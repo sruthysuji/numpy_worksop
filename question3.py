694fa5966255101255f21c8559866c53013d5c54
sruthysuji <153523151+sruthysuji@users.noreply.github.com>
2024-05-22 10:02:57 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,10 @@
 #write a program to find the factorial of a nummber
+def factorial(x):
+    if x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+num=int(input("enter a number : "))
+result =factorial(num)
+print("factorial of entered value is : ",result)
+ 
