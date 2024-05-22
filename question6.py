dcdaaf064601fffb5f3e2edb6591a63e6491c8de
sruthysuji <153523151+sruthysuji@users.noreply.github.com>
2024-05-22 10:15:47 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,7 @@
 #write a program to find the maximum of two numbers
+x=int(input("enter a number : "))
+y=int(input("enter a number : "))
+if x>y:
+       print("the greater number is ",x)
+else:
+       print("the greater number is ",y) 
